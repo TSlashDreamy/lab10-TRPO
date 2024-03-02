8f0cf76d71a2c5bc9c848fa6e5ef64bf00153994
TSlash <45912519+TSlashDreamy@users.noreply.github.com>
2024-03-02 22:44:57 +0200

    Add project files.

---
+++ lab10/Program.cs
@@ -0,0 +1,51 @@
+﻿// variant 8
+static double InBoundaries(double x)
+{
+    return 2 / (Math.Pow(x, 2) + 1);
+}
+
+static double OutsideBoundaries(double x)
+{
+    return Math.Abs(Math.Pow(x, 3) - 7) / x;
+}
+
+static IEnumerable<double> GenerateRange(double start, double end, double step)
+{
+    double current = start;
+    while (current <= end)
+    {
+        yield return current;
+        current += step;
+    }
+}
+
+Console.WriteLine("Your initial x value: ");
+double initialX = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Your end x value: ");
+double endX = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter tabulation step: ");
+double step = Convert.ToDouble(Console.ReadLine());
+
+List<double> results = new List<double>();
+
+Parallel.ForEach(GenerateRange(initialX, endX, step), x =>
+{
+    double result = (-3 <= x && x <= 3) ? InBoundaries(x) : OutsideBoundaries(x);
+
+    lock (results)
+    {
+        results.Add(result);
+    }
+});
+
+Console.WriteLine("Tabulation results: ");
+
+for (int i = 0; i < results.Count; i++)
+{
+    Console.WriteLine($"f({initialX + i * step}) = {results[i]}");
+}
+
+Console.WriteLine("\n\nPress any key to exit");
+Console.ReadKey();
